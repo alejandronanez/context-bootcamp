5e2cbe9b83b716e9022bbe0935fd36a1da9a5f25
Alejandro Nanez <alejonanez@gmail.com>
2018-06-03 17:29:52 -0500

    created basic Link component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,18 @@
 // @flow
 import React from 'react';
 import { PostsProvider } from 'providers/Post/PostProvider';
-import { RouteProvider, RouteConsumer } from 'providers/Route/RouteProvider';
+import { RouteProvider } from 'providers/Route/RouteProvider';
 import PostList from 'components/PostList/PostList';
 import PostItem from 'components/PostItem/PostItem';
 import Route from 'components/Route/Route';
+import Link from 'components/Link/Link';
 import { withCommon } from 'hoc/withCommon';
-
-const style = {
-  backgroundColor: '#ccc',
-  color: '#fff',
-  fontSize: '24px',
-  padding: '10px 20px',
-  display: 'inline-block',
-  marginRight: '10px',
-};
 
 const App = () => (
   <RouteProvider>
-    <RouteConsumer>
-      {context => (
-        <React.Fragment>
-          <div
-            style={style}
-            onClick={() => context.updateActiveScreen('posts')}
-          >
-            All posts
-          </div>
-          <div style={style} onClick={() => context.updateActiveScreen('post')}>
-            Individual post
-          </div>
-        </React.Fragment>
-      )}
-    </RouteConsumer>
     <PostsProvider>
+      <Link to="posts">All posts</Link>
+      <Link to="post">Individual post</Link>
       <Route activeScreen="posts">
         <PostList />
       </Route>
