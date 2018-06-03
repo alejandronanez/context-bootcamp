c6310b5e5847a1ffff8f7208eaf9dca4b5c1029f
Alejandro Nanez <alejonanez@gmail.com>
2018-06-03 12:41:48 -0500

    basic routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,21 @@
 // @flow
 import React from 'react';
 import { PostsProvider } from 'providers/Post/PostProvider';
+import { RouteProvider } from 'providers/Route/RouteProvider';
 import PostList from 'components/PostList/PostList';
+import { Route } from 'components/Route/Route';
 
 const App = () => (
-  <PostsProvider>
-    <PostList />
-  </PostsProvider>
+  <RouteProvider>
+    <PostsProvider>
+      <Route activeScreen="posts">
+        <PostList />
+      </Route>
+      <Route activeScreen="post">
+        <h1>One Post</h1>
+      </Route>
+    </PostsProvider>
+  </RouteProvider>
 );
 
 export default App;
